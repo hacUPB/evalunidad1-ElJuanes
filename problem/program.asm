e5588ad291cd5c560c1bd608681be88ee3de80ce
ElJuanes09 <juanex210404@gmail.com>
2022-09-03 18:34:10 -0500

    Linea#1

--- problem/program.asm
+++ problem/program.asm
@@ -2,3 +2,39 @@
 // 165728
 // juan.aristizabalo@upb.edu.co
 
+   @R0
+   D=M
+   @n
+   M=D
+
+   @i
+   M=0
+
+   @SCREEN
+   D=A
+   @adress
+   M=D
+(LOOP)
+   @i
+   D=M
+   @n
+   D=D-M
+   @END
+   D;JGT
+
+   @adress
+   A=M
+   M=-1
+
+   @i
+   M=M+1
+   @32
+   D=A
+   @adress
+   M=D+M
+   @LOOP
+   0;JMP
+
+(END)
+   @END
+
